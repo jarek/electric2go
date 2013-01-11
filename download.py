347fc34934ea93fd8fd8d69cbaac841305931815
Jarek Piórkowski <jarek@piorkowski.ca>
2013-01-10 16:02:03 -0800

    more crontab friendly - execute from anywhere in filesystem

--- download.py
+++ download.py
@@ -2,6 +2,7 @@
 # coding=utf-8
 
 import datetime
+import os
 import cars
 import index
 
@@ -9,14 +10,17 @@ city = cars.get_city()
 
 cars_text = cars.get_all_cars_text(city, force_download = True)
 
-f = open('./data/current_%s' % city, 'w')
+data_dir = os.path.dirname(os.path.realpath(__file__)) + '/data/'
+
+f = open(data_dir + 'current_%s' % city, 'w')
 print >> f, cars_text
 f.close()
 
 t = datetime.datetime.now()
-if t.minute % 10 == 0:
-	f = open('./data/%s_%04d-%02d-%02d--%02d-%02d' % (city, t.year, t.month, t.day, t.hour, t.minute), 'w')
+if t.minute % 5 == 0:
+	f = open(data_dir + '%s_%04d-%02d-%02d--%02d-%02d' % (city, t.year, t.month, t.day, t.hour, t.minute), 'w')
 	print >> f, cars_text
 	f.close()
 
+print str(datetime.datetime.now()),
 index.print_timer_info(cars.timer)
