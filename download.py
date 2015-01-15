005cbde47f5375e30c170768dc089720d3b589e8
Jarek Piórkowski <jarek@piorkowski.ca>
2015-01-15 20:18:14 +0000

    handle individual city data download failures in download.py

--- download.py
+++ download.py
@@ -17,22 +17,24 @@ else:
 
 t = datetime.datetime.now()
 for city in cities:
-    cars_text,cache = cars.get_all_cars_text(city, force_download = True)
+    try:
+        cars_text,cache = cars.get_all_cars_text(city, force_download = True)
+        current_filename = cars.data_dir + 'current_%s' % city
+        with open(current_filename, 'w') as f:
+            f.write(cars_text)
 
-    current_filename = cars.data_dir + 'current_%s' % city
-    f = open(current_filename, 'w')
-    print >> f, cars_text
-    f.close()
+        # save all information downloaded for now
+        #if cars.CITIES[city]['of_interest'] == True \
+        if True \
+                and t.minute % cars.DATA_COLLECTION_INTERVAL_MINUTES == 0:
+            filename = cars.filename_format % (city, t.year, t.month, t.day, t.hour, t.minute)
+            with open(cars.data_dir + filename, 'w') as f:
+                f.write(cars_text)
+    except:
+        # bypass cities that fail (like Ulm did in 2015-01) without killing whole script
+        print '!!! could not download or save information for city: %s' % city
+        continue
 
-    # save all information downloaded for now
-    #if cars.CITIES[city]['of_interest'] == True \
-    if True \
-        and t.minute % cars.DATA_COLLECTION_INTERVAL_MINUTES == 0:
-        filename = cars.filename_format % (city, \
-            t.year, t.month, t.day, t.hour, t.minute)
-        f = open(cars.data_dir + filename, 'w')
-        print >> f, cars_text
-        f.close()
 
 print str(t),
 index.print_timer_info(cars.timer)
