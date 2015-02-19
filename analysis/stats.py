21115488829d89802318463fc631f14ccfff436a
Jarek Piórkowski <jarek@piorkowski.ca>
2015-02-19 16:06:24 +0000

    WIP refactoring analysis/stats.py almost done
    Weird collecting weird trips is now cleaned up.
    Made a bunch of other clean-ups too.
    Still left: actually creating CSVs.

--- analysis/stats.py
+++ analysis/stats.py
@@ -45,12 +45,12 @@ def trace_vehicle(trips, criterion):
 
     return '\n'.join(lines)
 
-def print_stats(all_trips, all_known_vins, starting_time, ending_time,
-    weird_trip_distance_cutoff = 0.05, weird_trip_time_cutoff = 5,
-    # time cutoff should be 2 for 1 minute step data
-    weird_trip_fuel_cutoff = 1):
+def print_stats(all_trips, all_known_vins, starting_time, ending_time):
 
     def dataset_count_over(trips, thresholds, sorting_lambda=False):
+        """
+        :type sorting_lambda: function
+        """
         results = []
         for threshold in thresholds:
             if not sorting_lambda:
@@ -62,47 +62,17 @@ def print_stats(all_trips, all_known_vins, starting_time, ending_time,
 
         return results
 
-    def trip_breakdown(trips, sorting_bins = [120, 300, 600],
-        sorting_lambda = False, label = False):
-        # uses stats['total_trips'] defined before calling this subfunction
-        lines = []
-        for i in sorting_bins:
-            if not sorting_lambda:
-                trip_count = sum(1 for x in trips if x > i)
-            else:
-                trip_count = sum(1 for x in trips if sorting_lambda(x, i))
-
-            trip_percent = trip_count * 1.0 / stats['total_trips']
-
-            if not label:
-                line = 'trips over %d hours: %s' % \
-                    (i/60, trip_count_stats(trip_count))
-            else:
-                line = label(i, trip_count_stats(trip_count))
-            lines.append(line)
-        return '\n'.join(lines)
-
-    def trip_count_stats(trip_count):
-        # uses time_days and stats['total_trips'] defined 
-        # before calling this subfunction
-
-        if abs(time_days-1.0) < 0.01:
-            # don't calculate trips per day if value is within 1% of a day
-            # that's close enough that the differences won't matter in practice
-            return '%d (%0.4f of all trips)' % \
-                (trip_count, trip_count * 1.0 / stats['total_trips'])
-        else:
-            return '%d (%0.2f per day; %0.4f of all trips)' % \
-                (trip_count, trip_count/time_days, 
-                trip_count * 1.0 / stats['total_trips'])
-
-    def quartiles(collection, days=1.0):
+    def quartiles(collection, days):
         result = {}
         for i in range(0, 101, 25):
             result[i] = sps.scoreatpercentile(collection, i) / days
         return result
 
-    def get_stats(collection, collection_binned, days=1, over=False, most_common_count=10):
+    def get_stats(collection, collection_binned, days=1.0, over=False, under=False, most_common_count=10):
+        """
+        :type over: list
+        :type under: list
+        """
         result = OrderedDict()
         result['count all'] = len(collection)
         result['mean'] = np.mean(collection)
@@ -119,145 +89,149 @@ def print_stats(all_trips, all_known_vins, starting_time, ending_time,
             result['median per day'] = quartiles_per_day[50]
             result['quartiles per day'] = quartiles_per_day
 
-        if over:
-            result['thresholds'] = dataset_count_over(collection, over)
+        if over and result['count all'] > 0:
+            result['thresholds over'] = dataset_count_over(collection, over)
+
+        if under and result['count all'] > 0:
+            result['thresholds under'] = dataset_count_over(collection, under, sorting_lambda=lambda x, thr: x < thr)
 
         return result
 
     def format_stats(name, input_data):
+        """
+        Edit the stats dictionary slightly, formatting data to have less dicts/tuples
+        and more strings so it is easier to export.
+        Also prefix all keys with "name"
+        :param input_data: if 'thresholds over' or 'thresholds under' keys are included, 'count all' key must also be included
+        """
         result = OrderedDict()
 
         # format quartiles
         if 'quartiles' in input_data:
             for threshold, amount in input_data['quartiles'].items():
-                input_data['quartile %d' % threshold] = amount
+                input_data['quartile {}'.format(threshold)] = amount
             del input_data['quartiles']
 
         if 'quartiles per day' in input_data:
             for threshold, amount in input_data['quartiles per day'].items():
-                input_data['per day quartile %d' % threshold] = amount
+                input_data['per day quartile {}'.format(threshold)] = amount
             del input_data['quartiles per day']
 
         # format thresholds
-        if 'thresholds' in input_data:
-            for threshold, count_over in input_data['thresholds']:
-                input_data['over %d ratio' % threshold] = count_over * 1.0 / input_data['count all']
-            del input_data['thresholds']
+        if 'thresholds over' in input_data:
+            for threshold, count_over in input_data['thresholds over']:
+                input_data['over {} ratio'.format(threshold)] = count_over * 1.0 / input_data['count all']
+            del input_data['thresholds over']
+
+        if 'thresholds under' in input_data:
+            for threshold, count_over in input_data['thresholds under']:
+                input_data['under {} ratio'.format(threshold)] = count_over * 1.0 / input_data['count all']
+            del input_data['thresholds under']
 
         # prefix with name
-        for key, value in input_data.items():
-            result['%s %s' % (name, key)] = value
+        for input_key, input_value in input_data.items():
+            result['%s %s' % (name, input_key)] = input_value
 
         return result
 
-    def round_to(value, round_to):
-        return round_to*int(value*(1.0/round_to))
+    def duration(collection):
+        return [trip['duration']/60 for trip in collection]
 
-    data = {'total_trips': 0, 'trips': [],
-            'total_distance': 0, 'distances': [], 'distance_bins': [],
-            'total_duration': 0, 'durations': [], 'duration_bins': []}
-    weird = []
-    suspected_round_trip = []
-    refueled = []
+    def distance(collection):
+        return [trip['distance'] for trip in collection]
+
+    def fuel(collection):
+        return [trip['fuel_use'] for trip in collection]
+
+    def list_round(collection, round_to):
+        return [round_to * int(coll_value * (1.0 / round_to)) for coll_value in collection]
+
+    trips_weird = []
+    trips_good = []
+    trips_refueled = []
     trip_counts_by_vin = {}
     for trip in all_trips:
-        vin = trip['vin']
-        trip_counts_by_vin[vin] = trip_counts_by_vin.get(vin, 0) + 1
+        # Find and exclude "weird" trips, that are likely to be system errors caused by things like GPS misreads
+        # rather than actual trips.
+        # Not all errors will be caught - sometimes it is impossible to tell. Consequently,
+        # this operates on a best-effort basis, catching some of the most common and obvious problems.
+        # Various "weird" trips like that are somewhat less than 1% of a test dataset (Vancouver, Jan 27 - Feb 3)
+        # and the conditions below catch roughly 50-80% of them.
+        # TODO: these criteria are fairly car2go specific. They need to be tested on other systems.
+        if trip['duration'] < 4*60 and trip['distance'] <= 0.01 and trip['fuel_use'] > -2:
+            # trips under 4 minutes and under 10 metres are likely to be errors
+            trips_weird.append(trip)
+        elif trip['duration'] == 1*60 and trip['distance'] <= 0.05 and trip['fuel_use'] > -2:
+            # trips exactly 1 minute along and under 50 metres are likely to be errors
+            trips_weird.append(trip)
+        else:
+            trips_good.append(trip)
 
-        if trip['distance'] <= weird_trip_distance_cutoff:
-            suspected_round_trip.append(trip)
-            test_duration = weird_trip_time_cutoff * 60 # min to sec
-            #print trip
-            if trip['duration'] <= test_duration and \
-                trip['fuel_use'] <= weird_trip_fuel_cutoff:
-                # do not count this trip... it's an anomaly
-                #print 'weird'
-                weird.append(trip)
-                trip_counts_by_vin[vin] = trip_counts_by_vin[vin] - 1
-                continue
+            trip_counts_by_vin[trip['vin']] = trip_counts_by_vin.get(trip['vin'], 0) + 1
 
-        data['total_duration'] += trip['duration']/60
-        data['durations'].append(trip['duration']/60)
+            # TODO: also collect short distance but long duration and/or fuel use - these are likely to be round trips.
+            # Some sort of heuristic might have to be developed that establishes ratios of duration/fuel use
+            # that make a trip likely a round trip. Complicating matters is the fact that fuel use is quite unreliable.
 
-        # bin to nearest five minutes (if it isn't already)
-        data['duration_bins'].append(round_to(trip['duration']/60, 5))
-
-        data['total_distance'] += trip['distance']
-        data['distances'].append(trip['distance'])
-
-        # bin to nearest 0.5 km
-        data['distance_bins'].append(round_to(trip['distance'], 0.5))
-
-        if 'starting_fuel' in trip:
-            if trip['ending_fuel'] > trip['starting_fuel']:
-                refueled.append(trip)
+            if 'fuel_use' in trip and trip['fuel_use'] < 0:
+                # collect trips that have included a refuel, for use in stats to be added later
+                trips_refueled.append(trip)
 
     for vin in all_known_vins:
+        # fill in trip count for cars with 0 trips, if any
         if vin not in trip_counts_by_vin:
             trip_counts_by_vin[vin] = 0
 
-    # subtracting time_step below to get last file actually processed
-    time_elapsed = ending_time - starting_time
-    time_elapsed_seconds = time_elapsed.total_seconds()
-    time_days = time_elapsed_seconds * 1.0 / (24*60*60)
+    time_elapsed_seconds = (ending_time - starting_time).total_seconds()
+    time_elapsed_days = time_elapsed_seconds * 1.0 / (24*60*60)
 
-    data['trips'] = trip_counts_by_vin.values()
+    trips_per_car = trip_counts_by_vin.values()
 
     stats = OrderedDict()
-    stats['total_vehicles'] = len(trip_counts_by_vin)
-    stats['total_trips'] = len(all_trips) - len(weird)
-    stats['total_trips per day'] = stats['total_trips'] / time_days
+    stats['total vehicles'] = len(trip_counts_by_vin)
+    stats['total trips'] = len(trips_good)
+    stats['total trips per day'] = len(trips_good) / time_elapsed_days
 
-    stats['time_elapsed_seconds'] = time_elapsed_seconds
-    stats['time_elapsed_days'] = time_days
+    stats['time elapsed seconds'] = time_elapsed_seconds
+    stats['time elapsed days'] = time_elapsed_days
 
-    stats['utilization_ratio'] = data['total_duration'] / stats['total_vehicles'] / (time_elapsed_seconds/60)
+    stats['utilization ratio'] = sum(duration(trips_good)) / len(trip_counts_by_vin) / (time_elapsed_seconds/60)
 
     stats.update(format_stats('trips per car',
-                              get_stats(data['trips'], data['trips'], time_days)))
+                              get_stats(trips_per_car,
+                                        trips_per_car,
+                                        time_elapsed_days)))
 
     stats.update(format_stats('distance per trip',
-                              get_stats(data['distances'], data['distance_bins'], over=[5, 10])))
+                              get_stats(distance(trips_good),
+                                        list_round(distance(trips_good), 0.5),
+                                        over=[5, 10])))
 
     stats.update(format_stats('duration per trip',
-                              get_stats(data['durations'], data['duration_bins'], over=[120, 300, 600])))
+                              get_stats(duration(trips_good),
+                                        list_round(duration(trips_good), 5),
+                                        over=[2, 5, 10])))
+
+    stats.update(format_stats('fuel use stats',
+                              get_stats(fuel(trips_good),
+                                        fuel(trips_good),
+                                        under=[1, 5],
+                                        over=[1, 5, 10])))
+
+    # get some stats on weird trips as outlined above
+    stats['weird trip count'] = len(trips_weird)
+    stats['weird trips per day'] = len(trips_weird) * 1.0 / time_elapsed_days
+    stats['weird trip ratio'] = len(trips_weird) * 1.0 / len(all_trips)
+
+    stats.update(format_stats('weird trips duration',
+                              get_stats(duration(trips_weird),
+                                        duration(trips_weird))))
+    stats.update(format_stats('weird trips distance',
+                              get_stats(distance(trips_weird),
+                                        list_round(distance(trips_weird), 0.002),
+                                        under=[0.01, 0.02])))
 
     for key, value in stats.items():
         print '%s : %s' % (key, value)
 
-    # TODO: clean up code detecting weird trips and generating statistics from those as well.
     # TODO: return a dict suited for saving to CSV file rather than printing
-
-    # this is a weirdness with the datasets: 
-    # some have lots of short (<50 m, even <10m) trips.
-    # e.g., the austin sxsw has about 90 trips per day like that.
-    # unfortunately the API doesn't give us mileage, so hard to tell if
-    # it was a round trip, or if a car even moved at all.
-    # not sure what to do with them yet...
-    #weird_backup = weird
-    #weird = suspected_round_trip
-    durations = list(x['duration']/60 for x in weird)
-    distances = list(x['distance'] for x in weird)
-    distance_bins = list(int(1000*round_to(x['distance'], 0.005)) 
-        for x in weird)
-    fuel_uses = list(x['starting_fuel'] - x['ending_fuel'] for x in weird)
-
-    print '\ntrips reported shorter than %d m: %s' % \
-        (weird_trip_distance_cutoff * 1000, trip_count_stats(len(weird)))
-    print '\ndurations (minutes): mean %0.2f, stdev %0.3f, max %d' % \
-        (np.mean(durations), np.std(durations), max(durations))
-    print 'most common durations: %s' %Counter(durations).most_common(10)
-    print trip_breakdown(durations)
-    print '\ndistances (km): mean %0.4f, stdev %0.4f, max %0.2f' % \
-        (np.mean(distances), np.std(distances), max(distances))
-    print 'most common distances in metres, rounded to nearest 5 m: %s' % \
-        Counter(distance_bins).most_common(10)
-    print '\nfuel use (in percent capacity): mean %0.2f, stdev %0.3f, max %d' % \
-        (np.mean(fuel_uses), np.std(fuel_uses), max(fuel_uses))
-    print 'most common fuel uses: %s' % Counter(fuel_uses).most_common(10)
-    print trip_breakdown(fuel_uses, sorting_bins = [1, 5, 0],
-        sorting_lambda = (lambda x, i: (x > i) if i > 0 else (x < i)),
-        label = (lambda fuel, string: 
-            'more than %d pp fuel use: %s' % (fuel, string) if (fuel > 0) else
-            'refueled: %s' % string))
-
