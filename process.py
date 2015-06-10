jarek/electric2go

commit 262b021bcfb7d308abda84e148f37d35de381d44

remove a conditional that hasn't been used in ages
I've been using every-minute and 30fps since, like, 2013.
Remove the conditional that defaulted to every-5-minutes and 8 fps.

process.py
@@ -437,12 +437,9 @@ def batch_process(system, city, starting_time, dry = False, make_iterations = Tr
         png_filepaths = animation_files_prefix + '_%03d.png'
         mp4_path = animation_files_prefix + '.mp4'
 
-        framerate = 8
-        frames = total_frames - 1
-        if time_step < 5:
-            framerate = 30
-            # for framerates over 25, avconv assumes conversion from 25 fps
-            frames = (frames/25)*30
+        framerate = 30
+        # for framerates over 25, avconv assumes conversion from 25 fps
+        frames = (total_frames - 1/25)*framerate
 
         print '\nto animate:'
         print '''avconv -loop 1 -r %d -i %s -vf 'movie=%s [over], [in][over] overlay' -b 15360000 -frames %d %s''' % (framerate, background_path, png_filepaths, frames, mp4_path)

commit f74361e730eaca3e5a59aad0537e710ca49ea4fb

fix bug introduced in 262b021b; improve comment
Introduced in 262b021bcfb7d308abda84e148f37d35de381d44
I am a dummy

process.py
@@ -480,8 +480,11 @@ def batch_process(system, city, starting_time, dry = False, make_iterations = Tr
         mp4_path = animation_files_prefix + '.mp4'
 
         framerate = 30
-        # for framerates over 25, avconv assumes conversion from 25 fps
-        frames = (total_frames - 1/25)*framerate
+        # to my best understanding, my "input" is the static background image
+        # which avconv assumes to be "25 fps".
+        # to get output at 30 fps to be correct length to include all frames,
+        # I need to convert framecount from 25 fps to 30 fps
+        frames = ((total_frames - 1)/25)*framerate
 
         print('\nto animate:')
         print('''avconv -loop 1 -r %d -i %s -vf 'movie=%s [over], [in][over] overlay' -b 15360000 -frames %d %s''' % (framerate, background_path, png_filepaths, frames, mp4_path))