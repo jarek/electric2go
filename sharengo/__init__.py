a4d1cc39b476fb203783a7c0c89f77744333d308
Jarek Piórkowski <jarek@piorkowski.ca>
2015-06-22 12:44:48 +0000

    add download info for Share'NGo system in Milan

---
+++ sharengo/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python2
+# coding=utf-8
+
+
+# share'ngo auto-mobile has only one city for now, and the service always returns it,
+# so I am hardcoding the URL
+API_AVAILABLE_VEHICLES_URL = 'http://www.sharengo.it/cars'
