821e0c27377f5c2cc27f8a8b399e89542002b310
Jarek Piórkowski <jarek@piorkowski.ca>
2015-07-26 14:06:56 +0000

    updated Sharengo API format

--- sharengo/__init__.py
+++ sharengo/__init__.py
@@ -3,4 +3,4 @@
 
 # share'ngo auto-mobile has only one city for now, and the service always returns it,
 # so I am hardcoding the URL
-API_AVAILABLE_VEHICLES_URL = 'http://www.sharengo.it/cars'
+API_AVAILABLE_VEHICLES_URL = 'https://www.sharengo.it/core/publiccars'
--- sharengo/parse.py
+++ sharengo/parse.py
@@ -2,11 +2,11 @@
 
 
 def get_cars_from_json(json_data):
-    return json_data
+    return json_data['data'] if 'data' in json_data else json_data
 
 
 def extract_car_basics(car):
-    return car['plate'], car['latitude'], car['longitude']
+    return car['imei'], car['latitude'], car['longitude']
 
 
 def extract_car_data(car):
@@ -17,13 +17,27 @@ def extract_car_data(car):
     result['vin'] = vin
     result['lat'] = lat
     result['lng'] = lng
-    result['license_plate'] = vin
+    result['license_plate'] = car['plate']
 
     result['fuel'] = car['battery']
+    result['electric'] = True
+
+    result['address'] = ''
 
     result['model'] = car['model']
 
-    result['cleanliness_interior'] = car['int_cleanliness']
-    result['cleanliness_exterior'] = car['ext_cleanliness']
+    result['cleanliness_interior'] = car['intCleanliness']
+    result['cleanliness_exterior'] = car['extCleanliness']
+
+    result['charging'] = car['charging'] if 'charging' in car else False
 
     return result
+
+
+def get_range(car):
+    if 'fuel' not in car:
+        car = extract_car_data(car)
+
+    # Figures returned on API and website give a fairly linear 1% = 1.66 km ratio
+
+    return car['fuel'] * 1.66
