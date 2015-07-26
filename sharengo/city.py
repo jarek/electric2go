a013f70432cddbd5ee09c5810f32ac081f56d44b
Jarek Piórkowski <jarek@piorkowski.ca>
2015-07-26 09:56:49 +0000

    remove shebangs and +x from import-only files

--- sharengo/city.py
+++ sharengo/city.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python2
 # coding=utf-8
 
 from . import API_AVAILABLE_VEHICLES_URL
--- translink/city.py
+++ translink/city.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python2
 # coding=utf-8
 
 from . import API_ROUTE_ALL_BUSES_HEADERS, API_ROUTE_ALL_BUSES_URL
