2b2c218f13f4cbb01508a77b85a73111b497c53f
Jarek Piórkowski <jarek@piorkowski.ca>
2015-12-06 12:45:29 +0000

    move function only ever used in graph.py into that file
    I used to think I might need it elsewhere but I reconsidered and now it
    doesn't seem like it - it only calculates things in pixels, and only
    graph.py uses pixels.

--- analysis/graph.py
+++ analysis/graph.py
@@ -8,7 +8,6 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 from cars import get_city_by_name
-from city_helper import get_mean_pixel_size
 
 
 # speed ranges are designated as: 0-5; 5-15; 15-30; 30+
@@ -39,6 +38,31 @@ def is_latlng_in_bounds(city_data, latlng):
     is_lng = city_data['BOUNDS']['WEST'] <= lng <= city_data['BOUNDS']['EAST']
 
     return is_lat and is_lng
+
+
+def get_pixel_size(city_data):
+    # find the length in metres represented by one pixel on graph in both lat and lng direction
+
+    lat_range = city_data['MAP_LIMITS']['NORTH'] - city_data['MAP_LIMITS']['SOUTH']
+    lat_in_m = lat_range * city_data['DEGREE_LENGTHS']['LENGTH_OF_LATITUDE']
+    pixel_in_lat_m = lat_in_m / city_data['MAP_SIZES']['MAP_Y']
+
+    lng_range = city_data['MAP_LIMITS']['EAST'] - city_data['MAP_LIMITS']['WEST']
+    lng_in_m = lng_range * city_data['DEGREE_LENGTHS']['LENGTH_OF_LONGITUDE']
+    pixel_in_lng_m = lng_in_m / city_data['MAP_SIZES']['MAP_X']
+
+    return pixel_in_lat_m, pixel_in_lng_m
+
+
+def get_mean_pixel_size(city_data):
+    # find the length in metres represented by one pixel on graph
+
+    # take mean of latitude- and longitude-based numbers,
+    # which is not quite correct but more than close enough for most uses
+
+    pixel_in_m = get_pixel_size(city_data)
+
+    return (pixel_in_m[0] + pixel_in_m[1]) / 2
 
 
 def make_graph_axes(city_data, background=None, log_name=''):
