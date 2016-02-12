90483eb3c2b8eaa9340a46b495ece41938433509
Jarek Piórkowski <jarek@piorkowski.ca>
2016-02-12 12:08:18 +0000

    rename things that are strings, not parsed as json

--- electric2go/download.py
+++ electric2go/download.py
@@ -35,9 +35,9 @@ def download_one_city(city_data, session=None):
                            session,
                            city_data['API_AVAILABLE_VEHICLES_HEADERS'])
 
-    json_text, session = get_url(city_data['API_AVAILABLE_VEHICLES_URL'],
-                                 session,
-                                 city_data['API_AVAILABLE_VEHICLES_HEADERS'])
+    api_text, session = get_url(city_data['API_AVAILABLE_VEHICLES_URL'],
+                                session,
+                                city_data['API_AVAILABLE_VEHICLES_HEADERS'])
 
     # handle JSONP if necessary
     if 'JSONP_CALLBACK_NAME' in city_data:
@@ -45,17 +45,17 @@ def download_one_city(city_data, session=None):
         suffix1 = ');'
         suffix2 = ')'
 
-        json_text = json_text.decode('utf-8')
+        api_text = api_text.decode('utf-8')
 
-        if json_text.startswith(prefix):
-            if json_text.endswith(suffix1):
-                json_text = json_text[len(prefix):-len(suffix1)]
-            elif json_text.endswith(suffix2):
-                json_text = json_text[len(prefix):-len(suffix2)]
+        if api_text.startswith(prefix):
+            if api_text.endswith(suffix1):
+                api_text = api_text[len(prefix):-len(suffix1)]
+            elif api_text.endswith(suffix2):
+                api_text = api_text[len(prefix):-len(suffix2)]
 
-        json_text = json_text.encode('utf-8')
+        api_text = api_text.encode('utf-8')
 
-    return json_text, session
+    return api_text, session
 
 
 def save_one_city(city, timestamp_to_save, should_archive, session):
@@ -81,7 +81,7 @@ def get_current(city_data, max_cache_age):
     Gets current data for city. Returns data from local cache file
     if available, downloads data from API otherwise.
     """
-    json_text = None
+    api_text = None
     cache = False
 
     # see if it's already cached
@@ -92,14 +92,14 @@ def get_current(city_data, max_cache_age):
         if cached_data_age < max_cache_age:
             cache = cached_data_timestamp
             with open(cached_data_filename, 'rb') as f:
-                json_text = f.read()
+                api_text = f.read()
 
-    if not json_text:
+    if not api_text:
         cache = False
-        json_text, session = download_one_city(city_data)
+        api_text, session = download_one_city(city_data)
         session.close()
 
-    return json_text, cache
+    return api_text, cache
 
 
 def save(requested_system, requested_city, should_archive):
