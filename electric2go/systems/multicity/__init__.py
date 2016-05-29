9d5211e19a2c22729e7ad5bdd655c154758d5973
Jarek Piórkowski <jarek@piorkowski.ca>
2016-05-29 22:13:53 +0200

    code to get latlng extent for Drivenow and Multicity
    Update documentation in `adding new cities.howto` to take into account
    the new code, and improve it in general.

--- electric2go/systems/multicity/__init__.py
+++ electric2go/systems/multicity/__init__.py
@@ -12,3 +12,22 @@ CITIES = {
         'API_AVAILABLE_VEHICLES_URL': 'https://kunden.multicity-carsharing.de/kundenbuchung/hal2ajax_process.php?searchmode=buchanfrage&lat=52.51&lng=13.39&instant_access=J&ajxmod=hal2map&callee=getMarker&objectname=multicitymarker'
     }
 }
+
+
+def get_latlng_extent():
+    import requests
+    from lxml import etree
+    from ..drivenow import city as drivenow_city
+
+    # this URL is in https://www.multicity-carsharing.de/wp-content/plugins/multicity_map/multicity.js
+    # which as of 2016-05-29 is loaded by https://www.multicity-carsharing.de/
+    r = requests.get('https://www.multicity-carsharing.de/wp-content/plugins/multicity_map/geschaeftsbereich_07032014.kml')
+
+    xml = etree.fromstring(r.content)
+
+    ns = '{http://earth.google.com/kml/2.2}'
+    pl = xml.findall('.//' + ns + 'Placemark')
+
+    # reuse code from Drivenow to parse the KML
+    coords = drivenow_city.get_details_from_kml(pl[0], ns)
+    return drivenow_city.get_max_latlng(coords)
