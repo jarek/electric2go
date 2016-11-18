7b94dd0a83561f49a32e48b2784784fd5b14ac5a
Jarek Piórkowski <jarek@piorkowski.ca>
2016-11-18 18:05:59 +0100

    note Sharengo API weirdness

--- electric2go/systems/sharengo/__init__.py
+++ electric2go/systems/sharengo/__init__.py
@@ -2,6 +2,9 @@
 
 
 CITIES = {
+    # NOTE: Sharengo has one API endpoint, but as of 2016-11-18 it returns
+    # cars in three different cities/operating areas (Milan, Florence, Rome).
+    # Check car['fleet']['name'] or car['fleet']['code'] to distinguish them.
     'milano': {
         'of_interest': True,
         'electric': 'all',
