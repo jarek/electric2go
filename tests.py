jarek/electric2go

commit 640f2ca21d74785a8a1aafcb5af79ec56b96d37d

roll out parking drift before trip start - with test

tests.py
@@ -597,8 +597,6 @@ class GenerateTest(unittest.TestCase):
     # - Verify parking periods longer than a day are handled fine in generator
     # - Integration test using scripts/generate.py. Bonus: use py2 to generate
     #   then py3 to read-in, and vice versa, to ensure cross-version compatibility
-    # - Test stats for fuel use in a system that has electric cars that can charge
-    #   while parked. Sample: WBY1Z41070VZ77282 Duesseldorf trip 2016-08-20T17:28:00 to 18:08:00
 
     generated_data_dir = ''
 
@@ -711,12 +709,20 @@ class GenerateTest(unittest.TestCase):
 
         # read in data
         start = datetime(2016, 8, 20, 0, 0)
-        end = datetime(2016, 8, 20, 3, 0)
+        end = datetime(2016, 8, 20, 19, 0)
         freq = 60
 
         drivenow_original_data = normalize.batch_load_data(system, input_file, None, end, freq)
 
-        # generate 181 files covering from midnight to 3:00 every minute
+        # Bonus: test valid roll-out of parking drift data prior to creating trip.
+        # Test on a given trip in the dataset, found by looking at data manually.
+        trips = drivenow_original_data['finished_trips']['WBY1Z41070VZ77282']
+        sought_start_time = datetime(2016, 8, 20, 17, 28)
+        the_trip = [trip for trip in trips if trip['starting_time'] == sought_start_time]
+        self.assertEqual(len(the_trip), 1)
+        self.assertGreater(the_trip[0]['fuel_use'], 0)
+
+        # generate test files
         generated_data_dir = tempfile.mkdtemp()
         generate.write_files(drivenow_original_data, generated_data_dir)
 

electric2go/analysis/normalize.py
@@ -109,6 +109,16 @@ def process_data(parser, data_time, prev_data_time, available_cars, result_dict)
         result['starting_time'] = curr_time
         del result['ending_time']  # that was the ending time of the parking
 
+        # update with most recent changing data to cover "parking drift"
+        # (e.g. car changing during parking).
+        # otherwise, picking up a car that had been charging while parking
+        # would calculate 'fuel_use' from the value at the start of the parking
+        # (what is in 'fuel' key) and not at the end of the charging
+        # (what is in the last item of 'changing_data' key).
+        # there is always at least once value in changing_data list, per start_parking();
+        # the list contains tuples of (datetime, dataset) - we don't care about the datetime.
+        result = parser.put_car_parking_drift(result, result['changing_data'][-1][1])
+
         # at this point, `result` contains the output of parser.get_car_changing_properties
         # plus the following keys:
         # - from start_parking: vin, coords, starting_time (though overwritten just now), changing_data
@@ -129,8 +139,6 @@ def process_data(parser, data_time, prev_data_time, available_cars, result_dict)
         # just result = {'start': dict_comp_above} :)
         result = {key: result[key] for key in result
                   if key in {'start', 'starting_time', 'from'}}
-
-        # TODO: parking drift properties need to be rolled out, see comment in end_trip
 
         return result
 
@@ -160,13 +168,6 @@ def process_data(parser, data_time, prev_data_time, available_cars, result_dict)
         # update with data from the start of the trip
         trip_data = unfinished_trip
         trip_data.update(ending_trip_data)
-
-        # TODO: bug: picking up a car that had been charging while parking
-        # will calculate 'fuel_use' from the value at the start of the parking
-        # (what is in 'fuel' key) and not at the end of the charging
-        # (what is in the last item of 'changing_data' key).
-        # Need to roll out changing data when starting a trip probably.
-        # Sample: WBY1Z41070VZ77282 Duesseldorf trip 2016-08-20T17:28:00 to 18:08:00
 
         # calculate trip distance, duration, etc, based on start and end data
         trip_data = calculate_trip(trip_data)

electric2go/systems/car2go/parse.py
@@ -157,7 +157,7 @@ def get_car_parking_drift(car):
     :return: a hashable object
     """
 
-    charging = car['charging']
+    charging = car.get('charging', None)
 
     return car['fuel'], charging
 
@@ -172,7 +172,7 @@ def put_car_parking_drift(car, d):
 
     # TODO: needs testing with a system with electric cars. I don't think there are
     # any mixed systems anymore, so just test two systems separately
-    if car['fuel_type'] == 'ED':
+    if d[1]:
         car['charging'] = d[1]
 
     return car

commit 4957d216c8d405ff1d31f02724791ef59900b0cd

correct fuel use stats test data for car2go
This test started failing after 640f2ca21d, the commit that rolled out
parking drift before starting trips. I have looked in the source data
and it looks like the bug has been there all along, and has manifested
itself once I improved the normalizer.

The issue is caused by car2go API wobble, occasionally dropping reported
fuel level by 3% during a parking period. This tends to happen
particularly in the first minute after the parking period begins.
I've also seen fuel level jumping from 20-30% to 100% in the first
minute - perhaps the old value is given in the first minute sometimes?

This was previously being ignored but is now being picked up, changing
up fuel use stats slightly. Insert the correct values into the tests.

tests.py
@@ -170,8 +170,8 @@ class StatsTest(unittest.TestCase):
                     "trips per car median": 6.0,
                     "distance per trip quartile 25": 0.638860634765,
                     "duration per trip quartile 75": 32.0,
-                    "fuel use stats mean": -0.742084053,
-                    "fuel use stats over 5 ratio": 0.04663212435,
+                    "fuel use stats mean": -0.7559009787,
+                    "fuel use stats over 5 ratio": 0.04490500864,
                     "weird trip count": 64
                 },
                 "expected_dataframes": {
@@ -362,8 +362,8 @@ class StatsTest(unittest.TestCase):
                     "trips per car quartile 75": 14.0,
                     "distance per trip median": 1.76005701,
                     "duration per trip quartile 25": 15.0,
-                    "fuel use stats std": 9.581810626,
-                    "fuel use stats over 10 ratio": 0.003516624041,
+                    "fuel use stats std": 9.39346094,
+                    "fuel use stats over 10 ratio": 0.003356777494,
                     "weird trip ratio": 0.04648681603
                 },
                 "expected_dataframes": {